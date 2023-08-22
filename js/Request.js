9d8b7ee04c51eb9c1acefb3199a7f903c6e5b100
Cesar Gabriel <cesargabrielsousasousa196@gmail.com>
2023-08-22 06:54:11 -0300

    Alteração nas funções get E set

--- js/Request.js
+++ js/Request.js
@@ -1,10 +1,14 @@
+var Cars = []
+const carList = document.getElementById('carList');
+
 async function getCarList() {
         const response = await fetch('https://aprendendo-go.alphafox2.repl.co/GetCar');
 
         if (response.ok) {
             const cars = await response.json();
             const carListDiv = document.getElementById('carList');
-            carListDiv.textContent = JSON.stringify(cars, null, 2);
+            Cars = cars
+            carListDiv.textContent = JSON.stringify(Cars, null, 2);
         } else {
             console.error('Falha ao obter a lista de carros.');
         }
@@ -12,35 +16,43 @@ async function getCarList() {
 
 document.getElementById('addCarButton').addEventListener('click', async () => {
 
-    const newCar = {
-        ID: 1,
-        Name: "ÑovoCarro",
-        Price: 300,
-    };
+    var CarPrice = document.getElementById('Carprice').value
+    var CarName = document.getElementById('CarName').value
 
     const response = await fetch('https://aprendendo-go.alphafox2.repl.co/SetCar', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(newCar)
+        body: JSON.stringify({
+            Name: CarName,
+            Price: Number(CarPrice)
+        })
     });
 
     if (response.ok) {
         getCarList(); // Chama a função para atualizar a lista de carros após adicionar um novo carro
     } else {
         console.error('Falha ao adicionar o novo carro.');
+        console.log(newCar)
     }
 });
 
 document.getElementById('GetCarId0').addEventListener('click', async () => {
 
-    const response = await fetch('https://aprendendo-go.alphafox2.repl.co/GetCar/0');
+    var CardId = document.getElementById('CarId').value
+
+    if (CardId == null || CardId == " "){
+        console.error("Digite um Id")
+        return
+    }
+
+    const response = await fetch(`https://aprendendo-go.alphafox2.repl.co/GetCar/${CardId}`);
 
         if (response.ok) {
             const cars = await response.json();
-            const carListDiv = document.getElementById('carList');
-            carListDiv.textContent = JSON.stringify(cars, null, 2);
+            
+            carList.textContent = JSON.stringify(cars, null, 2);
         } else {
             console.error('Falha ao obter a lista de carros.');
         }
